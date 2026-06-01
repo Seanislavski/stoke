c292ad43301b3019db886a88d7c75a174d744192
Seanislavski <sean.rivis@gmail.com>
2026-05-31 22:41:19 -0400

    Hero: reduce fade distance to 150px for shorter desktop pages

--- apps/web/components/HomeHero.tsx
+++ apps/web/components/HomeHero.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from 'react'
 import StokeWordmark from '@/components/StokeWordmark'
 
-const FADE_PX = 300
+const FADE_PX = 150
 
 export default function HomeHero() {
   const heroRef = useRef<HTMLDivElement>(null)
